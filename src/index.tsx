c86eb76c1e92350c5984d1699d10901fe98c834d
waruhachi <156133757+waruhachi@users.noreply.github.com>
2024-09-10 13:13:31 -0500

    [+] initial commit with test code

--- src/index.tsx
+++ src/index.tsx
@@ -5,24 +5,65 @@ import { create } from 'enmity/patcher';
 import manifest from '../manifest.json';
 
 import Settings from './components/Settings';
-import { Text } from 'enmity/components';
-import { findInReactTree } from 'enmity/utilities';
+import { on } from 'events';
 
-const Stickers = getByProps('Stickers');
-const Patcher = create('Rain');
-
-console.log('Rain', Stickers);
+const Patcher = create(manifest.name);
+const ScrollView = getByProps('ScrollView').ScrollView;
+const FlatList = getByProps('ScrollView').FlatList;
+const SegmentedControlItem = getByProps(
+	'SegmentedControlItem'
+).SegmentedControlItem;
 
 const Rain: Plugin = {
 	...manifest,
 
 	onStart() {
-		// Patcher.after(Text.type, 'render', (self, args, res) => {
-		// 	const user = findInReactTree(
-		// 		res,
-		// 		(r) => r.props?.children[0][1].type.name == 'FriendPresence'
-		// 	);
-		// });
+		Patcher.before(ScrollView, 'render', (_, args, res) => {
+			console.log(args);
+			const key = args?.[0]?.children?.key;
+			if (key === '3') {
+				const parentView = args?.[0]?.children.props.children;
+
+				const RainSegmentedControlItem = React.createElement(
+					SegmentedControlItem,
+					{
+						style: {
+							minWidth: '25%',
+							marginStart: -4,
+							marginEnd: 4,
+						},
+						index: 3,
+						key: 'Rain',
+						label: 'Rain',
+						state: parentView[1][2].props.state,
+						pressed: parentView[1][2].props.pressed,
+						selected: false,
+						onPress: parentView[1][2].props.onPress,
+						onPressIn: parentView[1][2].props.onPressIn,
+						onPressOut: parentView[1][2].props.onPressOut,
+						icon: null,
+						variant: 'experimental_Small',
+					}
+				);
+
+				const newChild = {
+					...parentView[0].props.children[2],
+					key: 'Rain',
+				};
+
+				parentView[1].push(RainSegmentedControlItem);
+				parentView[0].props.children.push(newChild);
+				parentView[1].map((item) => {
+					item.props.style.minWidth = '25%';
+					item.props.state.items = [
+						{ id: 'Emoji', label: 'Emoji', page: null },
+						{ id: 'GIFs', label: 'GIFs', page: null },
+						{ id: 'Stickers', label: 'Stickers', page: null },
+						{ id: 'Rain', label: 'Rain', page: null },
+					];
+				});
+			}
+		});
 	},
 
 	onStop() {
